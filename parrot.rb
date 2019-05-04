27e397147f7687a3a683568abd41e0eb509b9438
Ranika Williams <rnkprice07@gmail.com>
2019-05-04 00:19:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  parrot="Squawk!"
+  puts "Squawk!"
+  return parrot
+end
+
+def parrot("Pretty bird!")
+  puts ("Pretty bird!")
+  return parrot
+end
